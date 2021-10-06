c3c8f24f189a75727f38064462bfb43ab1f6dc3b
Collyngodlewski <collyngodlewski@gmail.com>
2021-10-06 17:34:04 -0400

    added a constants

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { BASE_URL, API_KEY } from './constants';
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([])
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=FMeMhmd5QQbtlFm8JbfdBHNI8HD3qhrIPdsV52PP`)
+    axios.get(`${BASE_URL}/apod?api_key=${API_KEY}`)
     .then(res => {
       console.log(res);
       setData(res.data);
