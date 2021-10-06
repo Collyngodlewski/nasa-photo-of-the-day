206b243a8d0ca78f3f35d8ba1111aa984e88b11d
Collyngodlewski <collyngodlewski@gmail.com>
2021-10-06 17:18:59 -0400

    finished the normal mvp, going to mess with UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=FMeMhmd5QQbtlFm8JbfdBHNI8HD3qhrIPdsV52PP`)
+    .then(res => {
+      console.log(res);
+      setData(res.data);
+      return data;
+    }).catch(err =>{
+      console.error(err);
+    })
+  }, [])
+
+
+
+
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        Ooooo pretty space! <span role="img" aria-label='go!'>🚀</span>
       </p>
+      <img src={`${data.url}`} />
+      <p>{data.copyright}</p>
     </div>
   );
 }
